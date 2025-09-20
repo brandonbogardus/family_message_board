57fda803ef88d2bccf0362e789004f84c3d0a663
Brandon Bogardus <brandonbogardus@gmail.com>
2025-09-20 14:33:18 -0700

    Changed login/logout.

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "users"
 
 urlpatterns = [
-    path("logout/", views.custom_logout, name="logout"),  # Place this first!
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),  # Place this first!
     path("", include("django.contrib.auth.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,3 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 
-
-def custom_logout(request):
-    if request.method == "POST":
-        logout(request)
-        return redirect("message_board:message_board")  # Redirect after logout
-    return redirect("message_board:message_board")
